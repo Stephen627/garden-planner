811cd41e0225f2df5705c9e3ac9bb2e3eb6dcf34
Stephen James <spjames95@gmail.com>
2020-12-28 14:47:15 +0000

    test lang

---
+++ src/__tests__/utils/lang.test.ts
@@ -0,0 +1,11 @@
+import { __ } from '../../app/utils/lang';
+
+describe('Lang function', () => {
+    test('Searching for a word not the lang file will return the value provided', () => {
+        expect(__('this is a test phrase that doesn\'t exist')).toBe('this is a test phrase that doesn\'t exist');
+    });
+
+    test('Searching for a value that does exist should return something else', () => {
+        expect(__('test phrase exists')).toBe('something else');
+    });
+});
