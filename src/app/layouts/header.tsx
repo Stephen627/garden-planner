beca58c7facbb8e86b17d0b3d68d4f66c845fbdd
Stephen James <spjames95@gmail.com>
2021-01-13 10:36:16 +0000

    Update logo link to home page

--- src/app/layouts/header.tsx
+++ src/app/layouts/header.tsx
@@ -50,10 +50,10 @@ export class Header extends React.Component<HeaderProps, HeaderState> {
         return <header className="mx-w-7x1 mx-auto px-4 sm:px-6">
             <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
                 <div className="flex justify-start lg:w-0 lg:flex-1">
-                    <a href="#">
+                    <Link to={HOME_URL}>
                         <span className="sr-only">{__('Garden Planner')}</span>
                         <img className="h-8 w-auto sm:h-10" src={logo} alt="Logo"/>
-                    </a>
+                    </Link>
                 </div>
                 <div className="-mr-2 -my-2 md:hidden">
                     <button type="button" onClick={ () => this.setState({...this.state, showMobileMenu: true}) } className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-green-500">
@@ -78,9 +78,9 @@ export class Header extends React.Component<HeaderProps, HeaderState> {
                 <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
                     <div className="pt-5 pb-6 px-5">
                         <div className="flex items-center justify-between">
-                            <div>
+                            <Link to={HOME_URL}>
                                 <img className="h-8 w-auto" src={logo} alt="Logo"/>
-                            </div>
+                            </Link>
                             <div className="-mr-2">
                                 <button type="button" onClick={ () => this.setState({...this.state, showMobileMenu: false}) } className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-green-500">
                                     <span className="sr-only">Close menu</span>
