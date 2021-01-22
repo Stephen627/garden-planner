b0f802419a1d78353dfc6c2a2aec79aa6f240ce8
Stephen James <spjames95@gmail.com>
2021-01-22 12:32:20 +0000

    Update modal to tailwind

--- src/app/components/modal.tsx
+++ src/app/components/modal.tsx
@@ -4,22 +4,42 @@ import * as React from 'react';
 export interface ModalProps {
     children?: React.ReactChild;
     title: string;
-    onClose: Function;
+    submit: string;
+    cancel: string;
+    onSubmit: any;
+    onClose: any;
 }
 
 export const Modal = (props: ModalProps) => {
-    return <div className="modal">
-        <div className="modal__background"></div>
-        <div className="modal__content">
-            <div className="modal__close"><span onClick={() => props.onClose()}>X</span></div>
-            <div className="modal__title">
-                <h3>{props.title}</h3>
+    return <div className="fixed z-10 inset-0 overflow-y-auto">
+        <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+            <div className="fixed inset-0 transition-opacity" aria-hidden="true">
+                <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
             </div>
-            <div className="modal__inner-content">
-                {props.children}
+            <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+            
+            <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg w-full" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
+                <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                    <div className="sm:flex sm:items-start">
+                        <div className="mt-3 w-full text-center sm:mt-0 sm:ml-4 sm:text-left">
+                            <h3 className="text-lg leading-6 font-medium text-gray-900" id="modal-headline">{props.title}</h3>
+                            <div className="mt-2">
+                                {props.children}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                    <button type="button" onClick={props.onSubmit} className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-green-600 text-base font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:ml-3 sm:w-auto sm:text-sm">
+                        {props.submit}
+                    </button>
+                    <button type="button" onClick={props.onClose} className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
+                        {props.cancel}
+                    </button>
+                </div>
+                </div>
             </div>
         </div>
-    </div>
 }
 
 export default Modal;
