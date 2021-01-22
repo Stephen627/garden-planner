650fb61aa80ef0b335446e3eaeeb71c16d40fe60
Stephen James <spjames95@gmail.com>
2021-01-22 12:32:00 +0000

    Pass className to jsx

--- src/app/components/form/elements/text.tsx
+++ src/app/components/form/elements/text.tsx
@@ -5,7 +5,7 @@ export interface TextProps extends InputProps {
 }
 
 const Text = (props: TextProps) => {
-    return <input name={props.name} value={props.value || ''} type="text" placeholder={props.placeholder || ''} onChange={props.onChange}></input>
+    return <input className={props.className} name={props.name} value={props.value || ''} type="text" placeholder={props.placeholder || ''} onChange={props.onChange}></input>
 }
 
 export default Text;
