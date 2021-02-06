e3717c47b76d3476a934d4611a8e1b788a6aa17a
Stephen James <spjames95@gmail.com>
2021-02-06 11:13:44 +0000

    Change password

--- src/app/account/my-account/index.tsx
+++ src/app/account/my-account/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { Page } from '../../layouts/logged-in';
+import Password from './password';
 import Profile from './profile';
 
 const MyAccount = () => {
@@ -7,6 +8,7 @@ const MyAccount = () => {
         <div className="mx-6">
             <div className="md:grid md:grid-cols-3 md:gap-6">
                 <Profile/>
+                <Password/>
             </div>
         </div>
     </Page>
--- src/app/utils/authenticator/user/user-interface.ts
+++ src/app/utils/authenticator/user/user-interface.ts
@@ -1,4 +1,6 @@
 export default interface User {
     readonly uid: string;
+    readonly email: string;
     delete: () => Promise<void>;
+    updatePassword: (newPassword: string) => Promise<void>;
 }
