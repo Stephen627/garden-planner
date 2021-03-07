b9f9d8c9cf6f68ef3260cd1b2274a34cfc5e3fcd
Stephen James <spjames95@gmail.com>
2021-03-07 09:54:32 +0000

    Setup local adapter for db

--- src/app/utils/db.ts
+++ src/app/utils/db.ts
@@ -1,11 +1,21 @@
 
 import Database from './database';
 import FirebaseAdapter from './database/firebase/FirebaseDatabase';
+import LocalAdapater from './database/local/LocalDatabase';
+import Config from './config';
 
-const firebaseAdapter = new FirebaseAdapter();
 const db = new Database();
 
-db.setAdapter(firebaseAdapter);
+switch (Config.get('db_adapter')) {
+    case 'local':
+        const localAdapater = new LocalAdapater();
+        db.setAdapter(localAdapater);
+    case 'firebase':
+    default:
+        const firebaseAdapter = new FirebaseAdapter();
+        db.setAdapter(firebaseAdapter);
+        break;
+}
 
 export {
     db
