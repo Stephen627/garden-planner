d7eb5afc80175caea891b0dd2736052e7e136483
Stephen James <spjames95@gmail.com>
2021-03-07 09:55:59 +0000

    Correct spelling mistake

--- src/app/utils/db.ts
+++ src/app/utils/db.ts
@@ -1,15 +1,15 @@
 
 import Database from './database';
 import FirebaseAdapter from './database/firebase/FirebaseDatabase';
-import LocalAdapater from './database/local/LocalDatabase';
+import LocalAdapter from './database/local/LocalDatabase';
 import Config from './config';
 
 const db = new Database();
 
 switch (Config.get('db_adapter')) {
     case 'local':
-        const localAdapater = new LocalAdapater();
-        db.setAdapter(localAdapater);
+        const localAdapter = new LocalAdapter();
+        db.setAdapter(localAdapter);
     case 'firebase':
     default:
         const firebaseAdapter = new FirebaseAdapter();
