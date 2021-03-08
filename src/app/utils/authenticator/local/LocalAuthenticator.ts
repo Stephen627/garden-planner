c13ec72fe692d828ce29a3e10c3e40b4b41c2263
Stephen James <spjames95@gmail.com>
2021-03-08 07:44:54 +0000

    Local auth adapter

---
+++ src/app/utils/authenticator/local/LocalAuthenticator.ts
@@ -0,0 +1,52 @@
+import AuthenticatorInterface from '../authenticator-interface';
+import User from '../user/user-interface';
+import UserCredentials from '../user/user-credential-interface';
+
+
+class FirebaseAuthenticator implements AuthenticatorInterface {
+    private authenticated: boolean = false;
+    private onAuthChangeFunc: Function = null;
+
+    public onAuthChange (callback: any) {
+        this.onAuthChangeFunc = callback;
+    }
+
+    public isAuthenticated (): boolean {
+        return this.authenticated;
+    }
+
+    public currentUser (): User | null {
+        return {
+            uid: '',
+            email: '',
+            delete: () => { return new Promise(() => {})},
+            updatePassword: (newPassword: string) => { return new Promise((resolve) => { resolve(); })}
+        };
+    }
+
+    public authenticate (email: string, password: string): Promise<UserCredentials> {
+        return new Promise((resolve) => {
+            this.authenticated = true;
+            this.onAuthChangeFunc({});
+            resolve({});
+        });
+    }
+
+    public register (email: string, password: string): Promise<UserCredentials> {
+        return new Promise((resolve) => {
+            this.authenticated = true;
+            this.onAuthChangeFunc({});
+            resolve({});
+        });
+    }
+
+    public signout (): Promise<void> {
+        return new Promise((resolve) => {
+            this.authenticated = false;
+            this.onAuthChangeFunc({});
+            resolve();
+        });
+    }
+}
+
+export default FirebaseAuthenticator;
